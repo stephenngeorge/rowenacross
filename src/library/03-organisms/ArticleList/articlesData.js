93492f9d55aabdf41cda03015330853c0fcd04f8
Stephen Pal-George <stephenngeorge@gmail.com>
2020-01-29 20:21:52 +0000

    new article list data

--- src/library/03-organisms/ArticleList/articlesData.js
+++ src/library/03-organisms/ArticleList/articlesData.js
@@ -1,23 +1,11 @@
-// this is a basic data file for housing all the props data
-// that you will pass into this component. By default, loris has
-// setup a "data" object for you. You should import it in the file 
-// where you consume this component and then pass it to the relevant 
-// component like this: {...data}
-//
-// you can also add and export as many other objects from this file
-// as you like, this is a pattern we encourage. So if you had an instance
-// of this component on the homepage, setup and export a "homepage" object
-// and then an "aboutpage" object etc (or use whatever naming convention
-// you like!)
-
 const articlesData = [
   {
-    articleSummary: "'He sat down, called the twelve disciples over to him, and said, 'Whoever wants to be first must take last place and be the servant of everyone else.' - Mark 9:35. This was part of one of my readings today and it was one of the greatest lessons I learned and am still learning about being in any form of leadership.",
-    bodyText: "'He sat down, called the twelve disciples over to him, and said, 'Whoever wants to be first must take last place and be the servant of everyone else.' - Mark 9:35. This was part of one of my readings today and it was one of the greatest lessons I learned and am still learning about being in any form of leadership. I struggle with achieving it daily (oh let's be honest hourly) and I assumed it was because I am obviously a particularly selfish person. Which is oddly why I find Jesus' words so comforting in this passage. This verse is all about becoming a servant. Just like it says in Mark 10 (and Matthew and John such is its importance) where it says; 'For even the Son of Man did not come to be served, but to serve,' Did I really hear that? Jesus came not to be served but TO serve. Gets me EVERY time. If Jesus was a servant then why on earth would I not be. Why? Because it is completely counter cultural and goes exactly against what the world tells me and you every single day of our lives at every turn. The world says you are king, be in charge of your own life, love yourself first, make yourself happy, make your own rules and be master of your own destiny - in other words be your own god in your life. But Jesus (knowing exactly what the disciples had been arguing about in Mark 9:33) tells us to do the opposite. In fact in most of Jesus' teachings He says a variety of versions of these messages; Love God first. Love neighbour. Serve and not be served. Be last not first. Take care of the hungry, the prisoners the widows and orphans. Notice people. Share the Good News. Which when you look at them side by side is the EXACT opposite of our self focused culture. That is why it was hard for disciples then and why it's hard for us today now. Jesus is asking us to swim against the tide. Just like this picture I found. But I really wanted to share this because maybe like me you get thwarted by thinking 'I bet everyone else finds this easy so I must just be particularly selfish'. Which then makes trying to do this so easy to give up on. But that is a LIE of the enemy. With the exact intention of paralysing us into doing nothing. But Jesus KNOWS this about us and He loves us so He shows us the right way. Just like the light guiding the fish in the picture as it swims against the tide of people flooding towards him. So I say today embrace the fact we are self centred sometimes but Jesus sees our potential and believes in us. We don't have to stay that way. We do make mistakes but with Jesus, His Word and the Holy Spirit there is NOTHING we can't do or change about ourselves. I find embracing my frailty strangely freeing today. So I encourage you today to Swim against the tide today whatever your tide might be. Whether it's friends, fear, work, family, pain or fear again call on Jesus and serve and not be served. The beautiful thing is that by seeking to be the least it will actually make you a giant in His eyes.",
+    articleSummary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ipsum vitae ante pulvinar rutrum at eget ligula. Nullam blandit dapibus pharetra. Nulla vitae pretium diam. Pellentesque vehicula neque faucibus eros scelerisque auctor.",
+    bodyText: "He sat down, called the twelve disciples over to him, and said, 'Whoever wants to be first must take last place and be the servant of everyone else.' - Mark 9:35. This was part of one of my readings today and it was one of the greatest lessons I learned and am still learning about being in any form of leadership. I struggle with achieving it daily (oh let’s be honest hourly 🤣) and I assumed it was because I am obviously a particularly selfish person. Which is oddly why I find Jesus' words so comforting in this passage. This verse is all about becoming a servant. Just like it says in Mark 10 (and Matthew and John such is its importance) where it says; 'For even the Son of Man did not come to be served, but to serve,' Did I really hear that? Jesus came not to be served but TO serve. Gets me EVERY time. If Jesus was a servant then why on earth would I not be. Why? Because it is completely counter cultural and goes exactly against what the world tells me and you every single day of our lives at every turn. The world says you are king, be in charge of your own life, love yourself first, make yourself happy, make your own rules and be master of your own destiny- in other words be your own god in your life 😢. But Jesus (knowing exactly what the disciples had been arguing about in Mark 9:33) tells us to do the opposite. In fact in most of Jesus' teachings He says a variety of versions of these messages; Love God first, Love neighbour, Serve and not be served, Be last not first, Take care of the hungry, the prisoners the Widdows and orphans Notice people Share the Good News, which when you look at them side by side is the EXACT opposite of our self focused culture. That is why it was hard for disciples then and why it’s hard for us today now. Jesus is asking us to swim against the tide. Just like this picture I found. But I really wanted to share this because maybe like me you get thwarted by thinking 'I bet everyone else finds this easy so I must just be particularly selfish'. Which then makes trying to do this so easy to give up on. But that is a LIE of the enemy. With the exact intention of paralysing us into doing nothing. But Jesus KNOWS this about us and He loves us so He shows us the right way. Just like the light guiding the fish in the picture as it swims against the tide of people flooding towards him. So I say today embrace the fact we are self centred sometimes but Jesus sees our potential and believes in us. We don’t have to stay that way. We do make mistakes but with Jesus, His Word and the Holy Spirit there is NOTHING we can’t do or change about ourselves. I find embracing my frailty strangely freeing today 🔥💥. So I encourage you today to Swim against the tide today whatever your tide might be. Whether it’s friends, fear, work, family, pain or fear again call on Jesus and serve and not be served. The beautiful thing is that by seeking to be the least it will actually make you a giant in His eyes 💕",
     displayMode: "summary",
-    linkUrl: "/devotionals/the-first-must-be-last",
-    publishedDate: "02/12/2019",
-    titleText: "The first must be last",
+    linkUrl: "/devotionals/an-article",
+    publishedDate: "21/01/2020",
+    titleText: "Mark 9:35",
     id: 1
   },
   {
@@ -40,12 +28,4 @@ const articlesData = [
   }
 ]
 
-const data = {
-  // add props here that your component will expect in the format:
-  // PropName: prop value
-  articles: articlesData
-}
-
-export {
-  data
-}
+export default articlesData
