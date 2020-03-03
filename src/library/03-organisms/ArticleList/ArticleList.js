daa579c04254330ee89c7bebf7b8f3a41fd504d9
Stephen Pal-George <stephenngeorge@gmail.com>
2020-03-03 16:37:53 +0000

    tweak styles

--- src/library/03-organisms/ArticleList/ArticleList.js
+++ src/library/03-organisms/ArticleList/ArticleList.js
@@ -15,12 +15,24 @@ const ArticleList = ({
   additionalClasses,
   articles
 }) => {
-  const classes = ["article-list", ...additionalClasses]
+  const classes = [
+    "article-list",
+    articles.length <= 0 ? "no-results" : "",
+    ...additionalClasses
+  ]
   return (
     <div className={`${classes.join(" ")}`}>
       <section className="article-list__articles">
         {
+          articles.length > 0 &&
           articles.map(article => <Article key={ article.id } { ...article } />)
+        }
+        {
+          articles.length === 0 &&
+          <p className="article-list__no-results text-container--very-narrow color--light font-family--serif">
+            Sorry, we couldn't find anything for that search. Try entering something 
+            else or using one of the filters above.
+          </p>
         }
       </section>
     </div>
